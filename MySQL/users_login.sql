52f276362594616370f0f708c91f18793eb4a04b
JUANDAMO1004 <juandamo35@gmail.com>
2023-12-02 22:26:15 -0500

    First commit

---
+++ MySQL/users_login.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users_login(
+    Id INT PRIMARY KEY auto_increment,
+    Name VARCHAR (255),
+    LastName VARCHAR (255),
+    Username VARCHAR (255),
+    Password VARCHAR (255),
+    picture VARCHAR (255)
+);
